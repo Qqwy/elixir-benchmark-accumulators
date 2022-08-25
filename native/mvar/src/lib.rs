cbbc0adbfc202e1ace868c302bdf07c9563a855c
Marten/Qqwy <w-m@resilia.nl>
2022-08-25 10:09:04 +0200

    Using rustler_stored_term as optimization

--- native/mvar/src/lib.rs
+++ native/mvar/src/lib.rs
@@ -1,8 +1,7 @@
-use rustler::env::OwnedEnv;
-use rustler::env::SavedTerm;
 use rustler::resource::ResourceArc;
 use rustler::Env;
 use rustler::Term;
+use rustler_stored_term::StoredTerm;
 use std::sync::Mutex;
 
 mod atoms {
@@ -12,66 +11,66 @@ mod atoms {
 }
 
 pub struct MVar {
-    inner: Mutex<MVarContents>,
+    inner: Mutex<StoredTerm>,
 }
 
-struct MVarContents {
-    owned_env: OwnedEnv,
-    saved_term: SavedTerm,
-}
+// struct MVarContents {
+//     owned_env: OwnedEnv,
+//     saved_term: SavedTerm,
+// }
 
 impl MVar {
-    pub fn new(term: &Term) -> Self {
+    pub fn new(term: StoredTerm) -> Self {
         Self {
-            inner: Mutex::new(MVarContents::new(term)),
+            inner: Mutex::new(term),
         }
     }
 
-    pub fn get<'a>(&self, env: Env<'a>) -> Term<'a> {
-        self.inner.lock().unwrap().get(env)
+    pub fn get<'a>(&self) -> StoredTerm {
+        self.inner.lock().unwrap().clone()
     }
 
-    pub fn set(&self, term: &Term) {
-        self.inner.lock().unwrap().set(term)
+    pub fn set(&self, term: StoredTerm) {
+        *self.inner.lock().unwrap() = term
     }
 }
 
-impl MVarContents {
-    fn new(term: &Term) -> Self {
-        let owned_env = OwnedEnv::new();
-        let saved_term = owned_env.save(*term);
-        Self {
-            owned_env,
-            saved_term,
-        }
-    }
+// impl MVarContents {
+//     fn new(term: &Term) -> Self {
+//         let owned_env = OwnedEnv::new();
+//         let saved_term = owned_env.save(*term);
+//         Self {
+//             owned_env,
+//             saved_term,
+//         }
+//     }
 
-    fn get<'a>(&self, env: Env<'a>) -> Term<'a> {
-        self.owned_env.run(|owned_env| {
-            let term = self.saved_term.load(owned_env);
-            term.in_env(env)
-        })
-    }
+//     fn get<'a>(&self, env: Env<'a>) -> Term<'a> {
+//         self.owned_env.run(|owned_env| {
+//             let term = self.saved_term.load(owned_env);
+//             term.in_env(env)
+//         })
+//     }
 
-    fn set(&mut self, term: &Term) {
-        self.owned_env.clear();
-        self.saved_term = self.owned_env.save(*term);
-    }
+//     fn set(&mut self, term: &Term) {
+//         self.owned_env.clear();
+//         self.saved_term = self.owned_env.save(*term);
+//     }
+// }
+
+#[rustler::nif]
+fn new(term: StoredTerm) -> ResourceArc<MVar> {
+    ResourceArc::new(MVar::new(term))
 }
 
 #[rustler::nif]
-fn new(term: Term) -> ResourceArc<MVar> {
-    ResourceArc::new(MVar::new(&term))
+fn get(mvar: ResourceArc<MVar>) -> StoredTerm {
+    mvar.get()
 }
 
 #[rustler::nif]
-fn get(env: Env, mvar: ResourceArc<MVar>) -> Term {
-    mvar.get(env)
-}
-
-#[rustler::nif]
-fn set<'a>(env: Env<'a>, mvar: ResourceArc<MVar>, term: Term<'a>) -> Term<'a> {
-    mvar.set(&term);
+fn set(env: Env, mvar: ResourceArc<MVar>, term: StoredTerm) -> Term {
+    mvar.set(term);
     atoms::ok().to_term(env)
 }
 
